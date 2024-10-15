7bd7ef51d5a3484ceed9196a3afbb14f161be2c7
Timothee Lux <luxt@cardiff.ac.uk>
2024-10-15 15:43:11 +0100

    Merge sort algorithm added

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -28,6 +28,38 @@ def InsertionSort(nums):
         nums[j+1] = key
     return nums
 
-print(InsertionSort(nums = [2,4,3,1,5]))
+# print(InsertionSort(nums = [2,4,3,1,5]))
+
+def MergeSort(nums):
+    if len(nums) > 1:
+        mid = len(nums)//2
+        left = nums[0:mid]
+        right = nums[mid:len(nums)]
+
+        MergeSort(left)
+        MergeSort(right)
+
+        i = j = k = 0
+
+        while i<len(left) and j<len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i+=1
+            else:
+                nums[k] = right[j]
+                j+=1
+            k+=1
+        
+        while i<len(left):
+            nums[k] = left[i]
+            i+=1
+            k+=1
+        while j<len(right):
+            nums[k] = right[j]
+            j+=1
+            k+=1
+        return nums
+        
+print(MergeSort(nums = [2,4,3,1,5]))
 
 
